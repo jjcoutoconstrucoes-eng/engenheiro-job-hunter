3c6fbe074f655f2bf8d7fc8d57a2788fbd86cc2e
jjcoutoconstrucoes-eng <jj.coutoconstrucoes@gmail.com>
2026-08-21 10:07:06 -0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,43 @@
+CARGOS = [
+    "Engenheiro Orçamentista",
+    "Engenheiro de Custos",
+    "Engenheiro de Planejamento",
+    "Orçamentista de Obras",
+    "Analista de Orçamento",
+    "Analista de Custos",
+    "Engenheiro Civil",
+    "Planejamento e Controle de Obras",
+]
+
+LOCALIZACOES = [
+    "Rio de Janeiro",
+    "Niterói",
+    "São Gonçalo",
+]
+
+SALARIO_MINIMO = 10000
+SALARIO_ALVO = 12000
+
+REGIMES = [
+    "PJ",
+    "CLT",
+]
+
+NIVEL = [
+    "Pleno",
+    "Sênior",
+]
+
+SITES = [
+    "LinkedIn",
+    "Indeed",
+    "Gupy",
+    "InfoJobs",
+    "Catho",
+    "Glassdoor",
+    "Riovagas",
+    "Jobbol",
+    "Robert Half",
+    "Bebee",
+    "Sólides",
+]
